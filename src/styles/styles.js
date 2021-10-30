71d796dce712506d8d2bab75a5e760cbf2a4c7f1
Gabriel Rocha <gabriel.rocha@join4.com.br>
2021-10-29 23:56:42 -0300

    feat(home): implement styles and home page

---
+++ src/styles/styles.js
@@ -0,0 +1,28 @@
+import styled, { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+    body {
+        background-color: #080325;
+        color: #FFFFff;
+        overflow: hidden;
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica Neue, Arial, Noto Sans, Liberation Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol, Noto Color Emoji;
+    }
+`;
+
+export const ContentWrap = styled.section`
+  max-width: 1400px;
+  padding: 0.1px;
+  margin: 0 auto;
+  overflow: hidden;
+  background: #080325;
+  display: block;
+  box-sizing: border-box;
+`;
