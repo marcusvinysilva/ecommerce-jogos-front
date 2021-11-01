67a8b44496c6ff296dc14ba3dba625a32abeb3e5
Gabriel Rocha <gabriel.rocha@join4.com.br>
2021-11-01 20:54:30 -0300

    feat(pokemon): implement pokemon api just for tests

---
+++ src/context/gameContext.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react';
+import api from '../services/api';
+
+const GamesContext = createContext(null);
+
+const GamesProvider = ({ children }) => {
+    const [isFetch, setIsFetch] = useState(false);
+    const [games, setGame] = useState([]);
+    const [selectGame, setSelectGame] = useState(null);
+
+    const getGames = async () => {
+        setIsFetch(true)
+        try {
+            const { data } = await api.get('/pokemon');
+            setIsFetch(data);
+        } catch {
+            console.log('Something went wrong!')
+        } finally {
+            setIsFetch(false);
+        }
+    }
+
+    return (
+        <GamesContext.Provider
+            value={{
+                isFetch,
+                selectGame,
+                setGame,
+                games,
+                setSelectGame,
+                getGames,
+            }}
+        >
+            {children}
+        </GamesContext.Provider>
+    )
+}
+
+const useGames = () => {
+    return useContext(GamesContext);
+}
+
+export { GamesProvider, useGames}
