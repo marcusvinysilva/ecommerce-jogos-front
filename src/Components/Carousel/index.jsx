42d3ac4d8172d63bb7d73277d1acfccdd34c59c5
Walter Carvalho <walterclean@gmail.com>
2021-11-03 22:45:47 -0300

    feat (Banner): implements Banner and style

---
+++ src/Components/Carousel/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import BannerContainer from './styles'
+
+const Banner = () => {
+    return (
+        
+        <BannerContainer>
+            <div>
+        
+            <img  src="https://wp.nkdev.info/youplay/wp-content/uploads/2015/06/banner-bg.jpg" alt="Banner imagem"/>
+            
+            </div>
+        
+        </BannerContainer>
+        
+        
+    )
+}
+
+export default Banner
---
+++ src/Components/Carousel/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+    
+const BannerContainer = styled.div`
+    opacity:.5;
+    object-fit: cover;
+    object-position: 50% 50%;
+    max-width: 1400px;
+    top: 0px;
+    left: 60px;
+    width: 100%;
+    height: 517.6px;
+    overflow: hidden;
+    pointer-events: none;
+    margin-top: -10.8px;
+    transform: translate3d(0px, 10.84px, 0px);
+    vertical-align: middle;
+    border: 0;
+    aspect-ratio: auto 1400 / 645;
+    
+`
+export default BannerContainer;
