882aea6e3cc88bd05edba868668f55f6cb7fea8c
Vanessa de Sousa Lopes <83619433+nessaslopes@users.noreply.github.com>
2021-11-03 21:24:34 -0300

    Vai botão!

--- src/Components/GlobalButton/index.jsx
+++ src/Components/GlobalButton/index.jsx
@@ -1,6 +1,6 @@
 class LoggingButton extends React.Component {
    // Essa sintaxe garante que o `this` seja vinculado ao handleClick.
-   
+   // Usado o CodePen, do site https://pt-br.reactjs.org/docs/handling-events.html
    handleClick = () => {
      console.log('this is:', this);
    }
