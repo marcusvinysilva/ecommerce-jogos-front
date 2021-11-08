2638d6e57a040863131a405477e78e51ef294888
Walter Carvalho <walterclean@gmail.com>
2021-11-08 20:39:40 -0300

    feat (globalButton): globalButton implemented

--- src/Components/GlobalButton/index.jsx
+++ src/Components/GlobalButton/index.jsx
@@ -1,15 +1,15 @@
-function createButton()
-{
-     var btn = document.createElement('BUTTON');
-     var lbl = document.createTextNode();        
-     btn.appendChild(lbl);   
-     btn.onclick = function()
-     {
-        window.history.go(0);
-     }
-     document.body.appendChild(btn);    
+import React from 'react';
+import Button from './styles'
+import {Link} from "react-dom";
+const GlobalButton = () => {
+   return (
+      <div>
+        <div>
+         <Button onClick={() => { alert('clicado') }}> Learn More </Button>
+         </div>
+      </div>
+   )
 }
 
-createButton();
-
+export default GlobalButton;
 
