acafd6143c7e3a43e774e9f9662fa22a2486aa1e
Walter Carvalho <walterclean@gmail.com>
2021-11-08 20:48:23 -0300

    fix banner height

--- src/Components/Carousel/styles.js
+++ src/Components/Carousel/styles.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
     
 const BannerContainer = styled.div`
+    background-attachment: initial;
+    background-size: cover;
     opacity:.5;
     object-fit: cover;
     object-position: 50% 50%;
@@ -8,7 +10,7 @@ const BannerContainer = styled.div`
     top: 0px;
     left: 60px;
     width: 100%;
-    height: 517.6px;
+    height: 100%;
     overflow: hidden;
     pointer-events: none;
     margin-top: -10.8px;
