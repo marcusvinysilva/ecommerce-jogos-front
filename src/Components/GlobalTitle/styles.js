a994c0dc962f5cf261ffe8d46376740385c4bc05
Walter Carvalho <walterclean@gmail.com>
2021-11-08 20:41:14 -0300

    feat (GlobalTitle): GlobalTitle implemented

---
+++ src/Components/GlobalTitle/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    font-size: 30px;
+    line-height: 33px;
+    margin-bottom: 2.5rem;
+    color: #fff;
+    font-weight: 500;
+    text-transform: uppercase;
+
+
+    
+`;
+export default Title;
