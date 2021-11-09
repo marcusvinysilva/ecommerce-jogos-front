f80d7b10fbdd9b3e2b5a349f9e04f974a347a64b
code_spanol <85651178+spanol@users.noreply.github.com>
2021-11-09 03:34:38 -0300

    hotfix
    Modal voltou a funcionar.

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,22 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Logo } from "../Logo/index";
-import {LoginUser} from "../LoginUser/index"
+import { LoginUser } from "../LoginUser/index";
 import styled from "styled-components";
 import ReactModal from "react-modal";
-import {faUser,faCartPlus} from "@fortawesome/free-solid-svg-icons"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-
+import { faUser, faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const StyledHeader = styled.div`
   text-align: center;
   background-color: #ffffff;
 `;
 const Nav = styled.nav`
-  /* text-align: center;
-  justify-content: center;
-  background-color: #000f;
-  opacity: 1; */
-  /* background: #000; */
   background-image: url("https://th.bing.com/th/id/R.2d925b29bece786a05a68c5afc3c3e97?rik=lRrPx4eYIGHt%2fA&pid=ImgRaw&r=0");
   height: 90px;
   display: flex;
@@ -25,51 +19,18 @@ const Nav = styled.nav`
   opacity: 0.6;
 `;
 
-const SyledUlddOut = styled.ul`
-  /*c-dropdown*/
-  display: flex;
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-const StyledLi = styled.li`
-  -webkit-text-size-adjust: 100%;
-`;
-
-const Styleda = styled.a`
-color:#fff;
-display: flex;
-align-items: center;
-text-decoration: none;
-padding: 0 1rem;
-height: 100%;
-`
-
-const StyledLiModal = styled.li`
-  list-style: none;
-  padding-top: 15%;
-  padding-left: 35%;
-`;
-
-const Divbutton = styled.div`
-display: flex;
-align-items: center;
-right: 10px;
-`;
-
 const Div = styled.div`
-
-opacity: 0.8;
-/* right: 40px; */
-padding-right: 100px;
-/* width: 20px; */
-display:flex;
-align-items: center;
-margin-right: --24px;
-`
+  opacity: 0.8;
+  /* right: 40px; */
+  padding-right: 100px;
+  /* width: 20px; */
+  display: flex;
+  align-items: center;
+  margin-right: --24px;
+`;
 
 const Button = styled.button`
-/* border-radius: 4rem;
+  /* border-radius: 4rem;
 background: #261ce1;
 padding: 11px 11px;
 color: #fff;
@@ -80,7 +41,8 @@ padding-top: 45px;
 width: 20px;
 height: 100px;
 margin-right: 30px; */
-/* opacity: 0.8; */
+  /* opacity: 0.8; */
+  border: none;
 `;
 
 function Header() {
@@ -93,25 +55,25 @@ function Header() {
     //         <Logo />
     //       </StyledLi>
     //       <StyledLi>
-            // <a href="#" onClick={() => SetShowModal(true)}>
-            //   Login
-            // </a>
-            // <Modal
-            //   isOpen={ShowModal}
-            //   onRequestClose={() => SetShowModal(false)}
-            //   ariaHideApp={false}
-            //   style={{
-            //     overlay: {
-            //       justifyContent:"center",
-            //       width: "450px",
-            //       height: "400px",
-            //     },
-            //   }}
-            // >
-            //   <StyledLiModal>
-            //     <LoginUser />
-            //   </StyledLiModal>
-            // </Modal>
+    // <a href="#" onClick={() => SetShowModal(true)}>
+    //   Login
+    // </a>
+    // <Modal
+    //   isOpen={ShowModal}
+    //   onRequestClose={() => SetShowModal(false)}
+    //   ariaHideApp={false}
+    //   style={{
+    //     overlay: {
+    //       justifyContent:"center",
+    //       width: "450px",
+    //       height: "400px",
+    //     },
+    //   }}
+    // >
+    //   <StyledLiModal>
+    //     <LoginUser />
+    //   </StyledLiModal>
+    // </Modal>
     //       </StyledLi>
     //       {/* <Cart/>*/}
     //     </SyledUlddOut>
@@ -119,39 +81,40 @@ function Header() {
     // </StyledHeader>
 
     <Nav>
-    <a src="/">
-      <img src={Logo} alt="Logo"/>
-    </a>
-     <Div>
-    <Button href="#" onClick={() => SetShowModal(true)}>
-    <FontAwesomeIcon icon={faUser} />
-            </Button>
-           
+      <a src="/">
+        <img src={Logo} alt="Logo" />
+      </a>
+      <Div>
+        <Button href="#" onClick={() => SetShowModal(true)}>
+          <FontAwesomeIcon icon={faUser} />
+        </Button>
 
+        <Button>
+          <FontAwesomeIcon icon={faCartPlus} />
+        </Button>
+      </Div>
 
-<FontAwesomeIcon icon={faCartPlus}/>
-
-            </Div>
-            
-            <ReactModal
-              isOpen={ShowModal}
-              onRequestClose={() => SetShowModal(false)}
-              ariaHideApp={false}
-              style={{
-                overlay: {
-                  justifyContent:"center",
-                  width: "450px",
-                  height: "400px",
-                },
-              }}
-            >
-              
-                <LoginUser />
-
-            </ReactModal>
-            </Nav>
-
-  )
+      <ReactModal
+        isOpen={ShowModal}
+        onRequestClose={() => SetShowModal(false)}
+        ariaHideApp={false}
+        style={{
+          overlay: {
+            justifyContent: "center",
+            // width: "450px",
+            // height: "400px",
+          },
+        }}
+      >
+        <div>
+          <h1>ze</h1>
+          {/* <StyledLiModal> */}
+          {/* <LoginUser /> */}
+          {/* </StyledLiModal> */}
+        </div>
+      </ReactModal>
+    </Nav>
+  );
 }
 
 export default Header;
