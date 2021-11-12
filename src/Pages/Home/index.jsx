60c347d54a806b9f0b094e7e3c6dab91f3350330
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-11-12 20:27:35 -0300

    chore: unused blank spaces removed

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import GameList from '../../Components/GameList'
-import { Hero } from '../../Components/Hero'
+import React from 'react';
+import GameList from '../../Components/GameList';
+import { Hero } from '../../Components/Hero';
 export default function Home() {
-    return (
-        <section>
-            <Hero />
-            <GameList />
-        </section>
-    )
+  return (
+    <section>
+      <Hero />
+      <GameList />
+    </section>
+  );
 }
--- src/context/gameContext.js
+++ src/context/gameContext.js
@@ -4,48 +4,48 @@ import api from '../services/api'
 const GamesContext = createContext(null)
 
 const GamesProvider = ({ children }) => {
-    const [isFetch, setIsFetch] = useState(false);
-    const [games, setGame] = useState([]);
-    const [selectGame, setSelectGame] = useState(null)
-    const gameList = []
+  const [isFetch, setIsFetch] = useState(false)
+  const [games, setGame] = useState([])
+  const [selectGame, setSelectGame] = useState(null)
+  const gameList = []
 
-    const getGames = async () => {
-        setIsFetch(true)
-        try {
-            const { data } = await api.get('/games')
-            setGame(data)
-        } catch {
-            console.log('Something went wrong!')
-        } finally {
-            setIsFetch(false)
-        }
+  const getGames = async () => {
+    setIsFetch(true)
+    try {
+      const { data } = await api.get('/games')
+      setGame(data)
+    } catch {
+      console.error('Something went wrong!')
+    } finally {
+      setIsFetch(false)
     }
+  }
 
-    for(let i = 0; i < (games.length - 85); i++) {
-        const thumbnail = games[i].thumbnail;
-        const title = games[i].title;
-        gameList.push(<img src={thumbnail} alt={title} />)
-    }
+  for (let i = 0; i < games.length - 85; i++) {
+    const thumbnail = games[i].thumbnail
+    const title = games[i].title
+    gameList.push(<img src={thumbnail} alt={title} />)
+  }
 
-    return (
-        <GamesContext.Provider
-            value={{
-                isFetch,
-                selectGame,
-                setGame,
-                games,
-                gameList,
-                setSelectGame,
-                getGames,
-            }}
-        >
-            {children}
-        </GamesContext.Provider>
-    )
+  return (
+    <GamesContext.Provider
+      value={{
+        isFetch,
+        selectGame,
+        setGame,
+        games,
+        gameList,
+        setSelectGame,
+        getGames,
+      }}
+    >
+      {children}
+    </GamesContext.Provider>
+  )
 }
 
 const useGames = () => {
-    return useContext(GamesContext)
+  return useContext(GamesContext)
 }
 
 export { GamesProvider, useGames }
