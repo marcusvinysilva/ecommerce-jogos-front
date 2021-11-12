ac34d4a67f7f3e980d570bdaa47a6e3f2b793a3f
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-11-12 20:26:51 -0300

    feat(games): games list and games details page

---
+++ src/Pages/GameDetails/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function GameDetails() {
+  return (
+    <section>
+      <h1>Game Details Page</h1>
+    </section>
+  )
+}
---
+++ src/Pages/GamesList/index.jsx
@@ -0,0 +1,7 @@
+export default function GamesList() {
+  return (
+    <div>
+      <h1>Games List</h1>
+    </div>
+  )
+}
