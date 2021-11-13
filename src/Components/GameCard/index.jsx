d349de6e09c76caf678156f183b43caf0a4490a2
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-11-12 23:40:40 -0300

    refactor(game): `useEffect` and blank spaces

--- src/Components/GameCard/index.jsx
+++ src/Components/GameCard/index.jsx
@@ -1,24 +1,18 @@
 import React, { useEffect } from 'react';
 import { useGames } from '../../context/gameContext';
-import { Carousel } from '../Carousel'
+import { Carousel } from '../Carousel';
 
 export const Cards = () => {
   const { getGames, gameList } = useGames();
 
   useEffect(() => {
     getGames();
-  }, [])
+  });
   return (
-    <Carousel
-      show={4}
-      infiniteLoop
-    >
-      
+    <Carousel show={4} infiniteLoop>
       {gameList.map((game, index) => (
-        <div key={index}>
-          {game}
-        </div>
+        <div key={index}>{game}</div>
       ))}
     </Carousel>
-  )
-}
+  );
+};
