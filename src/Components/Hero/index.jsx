ac70cd671b2940155bed97a4aa8d066ea0b2574c
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-11-15 09:23:25 -0300

    chore: format unused  white spaces

--- src/Components/Hero/index.jsx
+++ src/Components/Hero/index.jsx
@@ -1,21 +1,24 @@
-import React from 'react'
-import { Container, Content, Parallax, InfoContainer, Info } from './style'
-export const Hero = () => {
-    return (
-        <Container>
-            <Content>
-                <Parallax>
-                    <img src="http://placehold.it/1400x550" alt="text" />
-                </Parallax>
-            </Content>
-            <InfoContainer>
-                <div>
-                    <Info>
-                        <h1>Team Ultimate</h1>
-                        <button>Shop Now</button>
-                    </Info>
-                </div>
-            </InfoContainer>
-        </Container>
-    )
-}
+import React from 'react';
+import { Container, Content, Parallax, InfoContainer, Info } from './style';
+export const Hero = ({ image, title }) => {
+  return (
+    <Container>
+      <Content>
+        <Parallax>
+          <img
+            src={image ? image : `http://placehold.it/1400x550`}
+            alt={title ? title : 'unknown'}
+          />
+        </Parallax>
+      </Content>
+      <InfoContainer>
+        <div>
+          <Info>
+            <h1>{title ? title : 'Ultimate'}</h1>
+            <button>Shop Now</button>
+          </Info>
+        </div>
+      </InfoContainer>
+    </Container>
+  );
+};
