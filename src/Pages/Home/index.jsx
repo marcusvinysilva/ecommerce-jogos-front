14946cdc8c2c5e07682c06d4f2754cc7b6e38ad9
Gabriel Rocha <gabrielrochaas@gmail.com>
2021-11-16 23:22:36 -0300

    refactor(header)!: update libs

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,15 +1,14 @@
-import styled from 'styled-components'
-import React from 'react'
-import { Footer } from '../../Components/Footer'
-import GameList from '../../Components/GameList'
-import Header from '../../Components/Header'
+import React from 'react';
+import { Footer } from '../../Components/Footer';
+import GameList from '../../Components/GameList';
+import Header from '../../Components/Header';
 
 export default function Home() {
-    return (
-        <section>
-            <Header />
-            <GameList />
-            <Footer />
-        </section>
-    )
+  return (
+    <section>
+      <Header />
+      <GameList />
+      <Footer />
+    </section>
+  );
 }
