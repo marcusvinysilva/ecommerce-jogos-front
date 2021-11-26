4ac63fe2136c42d7ba7fef8cffe0c9d2793eb8c2
Walter Carvalho <walterclean@gmail.com>
2021-11-26 11:19:43 -0300

    feat{Hero}: Fix  hero image at shopList

--- src/Pages/ShopList/index.jsx
+++ src/Pages/ShopList/index.jsx
@@ -1,21 +1,37 @@
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { Breadcrumbs } from '@mui/material';
+
+import { useGames } from '../../context/GameContext';
+
+import { GamesShopList } from '../../Components/ShopList';
+import { Hero } from '../../Components/Hero';
+import { Footer } from '../../Components/Footer';
+import Header from '../../Components/Header';
+
 import {
-  Container,
-  BreadcrumbsAndForm,
-  Content,
-  Form,
+Container,
+BreadcrumbsAndForm,
+Content,
+Form,
   Main,
   Row,
 } from './style';
-import { GamesShopList } from '../../Components/ShopList';
-import { Hero } from '../../Components/Hero';
-import { Breadcrumbs } from '@mui/material';
-import Header from '../../Components/Header';
-import { Footer } from '../../Components/Footer';
+
+
+
 export default function ShopList() {
+  const { selectedGame, getSelectedGame } = useGames();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getSelectedGame(id);
+  }, []);
   return (
     <section>
       <Header />
-      <Hero />
+      <Hero image={selectedGame.images} title={selectedGame.gameName} />
       <Container className="container">
         <Row className="row">
           <Main className="main">
